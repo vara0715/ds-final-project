012eab9aef95268efd5640cd97634fb1a76ceb15
vara0715 <88980746+vara0715@users.noreply.github.com>
2023-01-05 11:30:17 +0530

    Update food ordering app.py

--- food ordering app.py
+++ food ordering app.py
@@ -313,6 +313,46 @@ class new_order(order):
         
 
 obj = new_order()
+###User Main....
+#   to get all the stuff user want to run the program !!!
+#  Start with registration
+from _user_registration import registration
+
+#  please refer to user registration for login username and password else do registration !!!!
+registration()
+
+# Giving option to place order,view order history,update profile
+def choice():
+    option = int(input("\n1-Place New Order \n2-Order History \n3-Update Profile\n"))
+    
+    # For Placing New Order
+    if option == 1:
+        from _new_order import new_order
+        obj = new_order()
+        obj.new_order()
+
+    # For Order History
+    elif option == 2:
+        try:
+            file = open("Order.txt",'r+')
+            print(file.read())
+            file.close()        
+            choice()
+        except:
+            print("Please order first if you are using the app for the first time")
+            choice()
+
+    # For Updating The Profile
+    elif option == 3:
+        from _Update_Profile import update_profile
+        update_profile()
+    
+    # If Someone Choose Other option Rather Than 1,2,3  
+    else:
+        print("Seclect Valid information")
+        choice()
+
+choice()
 ##User Registation
 import re
 
